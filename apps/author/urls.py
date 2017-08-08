c13b31497b095ab22fbaae3b439b72b1c9e05e86
Oscar Llorente <llorente_oscar@hotmail.com>
2017-08-07 20:04:49 -0500

    Arreglo de los links mas frecuentes

--- apps/author/urls.py
+++ apps/author/urls.py
@@ -7,7 +7,13 @@ from .views import *
 
 
 urlpatterns = [
-    url(r'^(?P<v>\d+)/$', indexAutores, name='vista_autores'),
-	url(r'^(?P<v>[\w-]+)/(?P<w>\d+)/$', indexAutor, name='vista_autor'),
+    url(r'^$', indexHome, name='vista_home'),
+    url(r'^configuracion/$', indexPost_crear, name='vista_configuracion'),
+    url(r'^crear-post/$', indexPost_crear, name='vista_post_crear'),
+    url(r'^autor/(?P<v>\d+)/$', indexAutores, name='vista_autores'),
+    url(r'^autor/(?P<v>[\w-]+)/(?P<w>\d+)/$', indexAutor, name='vista_autor'),
+    url(r'^blog/(?P<v>\d+)/$', indexBlog, name='vista_blog'),
+    url(r'^editar/(?P<v>[\w-]+)/(?P<w>\d+)/(?P<x>[\w-]+)/$', indexPost_editar, name="vista_post_editar"),
+    url(r'^(?P<u>[\w-]+)/(?P<v>\d+)/$', indexCategoria, name='vista_categoria'),
     url(r'^(?P<u>[\w-]+)/(?P<v>\d+)/(?P<w>[\w-]+)/$', indexPost, name="vista_post"),
 ]
--- apps/author/views.py
+++ apps/author/views.py
@@ -9,39 +9,103 @@ from django.core.paginator import Paginator, EmptyPage, InvalidPage
 from .models import *
 
 
+def indexHome(request):
+	categorias = Categoria.objects.filter(menu=1, activo=True, bloqueo=True).order_by('titulo')
+	# pricategoria = Categoria.objects.get(id=5, activo=True, bloqueo=True)
+	# principal = Post.objects.filter(categoria=pricategoria.id, activo=True, bloqueo=True).order_by('-fechacreado')[:3]
+	notcategoria = Categoria.objects.get(id=1, activo=True, bloqueo=True)
+	noticia = Post.objects.filter(categoria=notcategoria.id, activo=True, bloqueo=True).order_by('-fechacreado')[:3]
+	opicategoria = Categoria.objects.get(id=4, activo=True, bloqueo=True)
+	opinion = Post.objects.filter(categoria=opicategoria.id, activo=True, bloqueo=True, autor__is_active=True, autor__is_staff=False).order_by('-fechacreado')[:3]
+	# alocategoria = Categoria.objects.get(id=3, activo=True, bloqueo=True)
+	# aloido = Post.objects.filter(categoria=alocategoria.id, activo=True, bloqueo=True).order_by('-fechacreado')[:3]
+	# humcategoria = Categoria.objects.get(id=4, activo=True, bloqueo=True)
+	# humor = Post.objects.filter(categoria=humcategoria.id, activo=True, bloqueo=True).order_by('-fechacreado')[:3]
+	regcategoria = Categoria.objects.get(id=5, activo=True, bloqueo=True)
+	region = Post.objects.filter(categoria=regcategoria.id, activo=True, bloqueo=True).order_by('-fechacreado')[:3]
+	# intcategoria = Categoria.objects.get(id=7, activo=True, bloqueo=True)
+	# internacional = Post.objects.filter(categoria=intcategoria.id, activo=True, bloqueo=True).order_by('-fechacreado')[:3]
+	# vidcategoria = Categoria.objects.get(id=9, activo=True, bloqueo=True)
+	# video = Post.objects.filter(categoria=vidcategoria.id, activo=True, bloqueo=True).order_by('-fechacreado')[:3]
+	# entcategoria = Categoria.objects.get(id=8, activo=True, bloqueo=True)
+	# entrevista = Post.objects.filter(categoria=entcategoria.id, activo=True, bloqueo=True).order_by('-fechacreado')[:3]
+	# percategoria = Categoria.objects.get(id=5, activo=True, bloqueo=True)
+	# personaje = Post.objects.filter(categoria=percategoria.id, activo=True, bloqueo=True).order_by('-fechacreado')[:3]
+	# ecocategoria = Categoria.objects.get(id=10, activo=True, bloqueo=True)
+	# ecosbogota = Post.objects.filter(categoria=ecocategoria.id, activo=True, bloqueo=True).order_by('-fechacreado')[:3]
+	# teccategoria = Categoria.objects.get(id=11, activo=True, bloqueo=True)
+	# tecnologia = Post.objects.filter(categoria=teccategoria.id, activo=True, bloqueo=True).order_by('-fechacreado')[:3]
+	ecncategoria = Categoria.objects.get(id=2, activo=True, bloqueo=True)
+	economia = Post.objects.filter(categoria=ecncategoria.id, activo=True, bloqueo=True).order_by('-fechacreado')[:3]
+	link = Post.objects.filter(activo=True, bloqueo=True).order_by('-vistas')[:10]
+	us = 'Home'
+	context = {
+		'categorias':categorias,
+		# 'pricategoria':pricategoria,
+		# 'principal':principal,
+		'notcategoria':notcategoria,
+		'noticia':noticia,
+		'opicategoria':opicategoria,
+		'opinion':opinion,
+		# 'alocategoria':alocategoria,
+		# 'aloido':aloido,
+		# 'humcategoria':humcategoria,
+		# 'humor':humor,
+		'regcategoria':regcategoria,
+		'region':region,
+		# 'intcategoria':intcategoria,
+		# 'internacional':internacional,
+		# 'vidcategoria':vidcategoria,
+		# 'video':video,
+		# 'entcategoria':entcategoria,
+		# 'entrevista':entrevista,
+		# 'percategoria':percategoria,
+		# 'personaje':personaje,
+		# 'ecocategoria':ecocategoria,
+		# 'ecosbogota':ecosbogota,
+		# 'teccategoria':teccategoria,
+		# 'tecnologia':tecnologia,
+		'ecncategoria':ecncategoria,
+		'economia':economia,
+		'link':link,
+		'us':us,
+	}
+	return render(request, 'home/index.html', context)
+
+
 def indexAutores(request, v):
+    categorias = Categoria.objects.filter(menu=1, activo=True, bloqueo=True).order_by('titulo')
+    autor = Perfil.objects.filter(activo=True, bloqueo=True, usuario__is_active=True, usuario__is_staff=False).order_by('usuario__first_name')
+    link = Post.objects.filter(activo=True, bloqueo=True).order_by('-vistas')[:10]
+    paginator = Paginator(autor, 10)
     try:
-        autor = User.objects.filter(is_active=True, is_staff=False).order_by('first_name')
-        perfil = Perfil.objects.get(usuario=autor, activo=True, bloqueo=True)
-        paginator = Paginator(autor, 10)
-    	try:
-    		page = int(v)
-    	except:
-    		page = 1
-    	try:
-    		autor = paginator.page(page)
-    	except (EmptyPage, InvalidPage):
-    		autor = paginator.page(paginator.num_pages)
-    	us = 'Home'
-        uslink = '/'
-        vs = 'Autor'
-        ws = page
-        context = {
-            'autor':autor,
-            'perfil':perfil,
-            'us':us,
-            'uslink':uslink,
-            'vs':vs,
-            'ws':ws,
-        }
-        return render(request, 'autor/index.html', context)
-    except autor.DoesNotExist:
-        raise Http404("Autor does not exist")
+        page = int(v)
+    except:
+        page = 1
+    try:
+        autor = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        autor = paginator.page(paginator.num_pages)
+    us = 'Home'
+    uslink = '/'
+    vs = 'Autor'
+    ws = v
+    context = {
+        'categorias':categorias,
+        'autor':autor,
+        'link':link,
+        'us':us,
+        'uslink':uslink,
+        'vs':vs,
+        'ws':ws,
+    }
+    return render(request, 'autor/index.html', context)
 
 
 def indexAutor(request, v, w):
+    categorias = Categoria.objects.filter(menu=1, activo=True, bloqueo=True).order_by('titulo')
     autor = User.objects.get(username=v, is_active=True, is_staff=False)
-    perfil = Perfil.objects.get(usuario=autor, activo=True, bloqueo=True)
+#    perfil = Perfil.objects.get(usuario=autor, activo=True, bloqueo=True)
     post = Post.objects.filter(autor=autor, activo=True, bloqueo=True)
     postcount = Post.objects.filter(autor=autor, activo=True, bloqueo=True).count()
     titulo = User.objects.get(username=v, is_active=True, is_staff=False)
@@ -62,9 +126,10 @@ def indexAutor(request, v, w):
     ws = autor.first_name + ' ' + autor.last_name
     xs = page
     context = {
+        'categorias':categorias,
         'autor':autor,
         'link':link,
-        'perfil':perfil,
+#        'perfil':perfil,
         'post':post,
         'postcount':postcount,
         'titulo':titulo,
@@ -78,48 +143,37 @@ def indexAutor(request, v, w):
     return render(request, 'autor/autor.html', context)
 
 
-def indexPost(request, u, v, w):
-#	if request.user.is_authenticated():
-#		url = '/' + u + '/' + v + '/' + w + '/'
-#		form = ComentarioForm()
-#		if request.method == "POST":
-#			form = ComentarioForm(request.POST)
-#			if form.is_valid():
-#				form.save()
-#				post = form.cleaned_data['post']
-#				usuario = form.cleaned_data['usuario']
-#				comentario = form.cleaned_data['comentario']
-#				fecha = form.cleaned_data['fecha']
-#				u = Post.objects.creationform(post=post, usuario=usuario, comentario=comentario, fecha=fecha)
-#				u.save()
-#				return HttpResponseRedirect(url)
-#			else:
-#				return HttpResponseRedirect(url)
+def indexPost_crear(request):
+    cat = Categoria.objects.filter(activo=True, bloqueo=True).order_by('titulo')
+    us = 'Home'
+    uslink = '/'
+    vs = 'Crear post'
+    context = {
+        'cat':cat,
+        'us':us,
+        'uslink':uslink,
+        'vs':vs,
+    }
+    return render(request, 'post/post_crear.html', context)
 
-	cat = Categoria.objects.filter(menu=1)
+
+def indexPost(request, u, v, w):
+	categorias = Categoria.objects.filter(menu=1, activo=True, bloqueo=True).order_by('titulo')
 	acceso = Tipoacceso.objects.all()
 	categoria = Categoria.objects.get(slug=u, activo=True, bloqueo=True)
-	categorias = Categoria.objects.filter(menu=1, activo=True, bloqueo=True).order_by('titulo')
 	post = Post.objects.get(id=v, activo=True, bloqueo=True)
-#	comentario = Comentario.objects.filter(post=v)[:10]
-#	comentariocount = Comentario.objects.filter(post=v).count()
 	link = Post.objects.filter(categoria=categoria.id, activo=True, bloqueo=True).order_by('-vistas')[:10]
-#	megustacount = Megusta.objects.filter(graf=v).count()
 	us = 'Home'
 	uslink = '/'
 	vs = categoria.titulo
 	vslink = '/' + categoria.slug + '/1/'
 	ws = post.titulo
 	context = {
-		'cat':cat,
+        'categorias':categorias,
 		'acceso':acceso,
 		'categoria':categoria,
-		'categorias':categorias,
 		'post':post,
-#		'comentario':comentario,
-#		'comentariocount':comentariocount,
 		'link':link,
-#		'megustacount':megustacount,
 		'us':us,
 		'uslink':uslink,
 		'vs':vs,
@@ -127,3 +181,130 @@ def indexPost(request, u, v, w):
 		'ws':ws,
 	}
 	return render(request, 'post/index.html', context)
+
+
+def indexPost_editar(request, v, w, x):
+    categorias = Categoria.objects.filter(menu=1, activo=True, bloqueo=True).order_by('titulo')
+    categoria = User.objects.get(username=v, is_active=True, is_staff=False)
+    cat = Categoria.objects.filter(activo=True, bloqueo=True).order_by('titulo')
+    post = Post.objects.get(id=w, activo=True, bloqueo=True)
+    link = Post.objects.filter(activo=True, bloqueo=True).order_by('-vistas')[:10]
+    us = 'Home'
+    uslink = '/'
+    vs = categoria.first_name + ' ' + categoria.last_name
+    vslink = '/autor/' + categoria.username + '/1/'
+    ws = 'Editar'
+    wslink = '/backoffice/mis-post/1/'
+    xs = post.titulo
+    context = {
+        'categorias':categorias,
+        'cat':cat,
+        'post':post,
+        'link':link,
+        'us':us,
+        'uslink':uslink,
+        'vs':vs,
+        'vslink':vslink,
+        'ws':ws,
+        'wslink':wslink,
+        'xs':xs,
+    }
+    return render(request, 'post/post_editar.html', context)
+
+
+def indexCategoria(request, u, v):
+	categoria = Categoria.objects.get(slug=u, activo=True, bloqueo=True)
+	categorias = Categoria.objects.filter(menu=1, activo=True, bloqueo=True).order_by('titulo')
+	posts = Post.objects.filter(categoria=categoria.id, activo=True, bloqueo=True).order_by('-fechacreado')
+	paginator = Paginator(posts, 10)
+	try:
+		page = int(v)
+	except:
+		page = 1
+	try:
+		post = paginator.page(page)
+	except (EmptyPage, InvalidPage):
+		post = paginator.page(paginator.num_pages)
+	link = Post.objects.filter(categoria=categoria.id, activo=True, bloqueo=True).order_by('-vistas')[:10]
+	link2 = Post.objects.filter(activo=True, bloqueo=True).order_by('-vistas')[:10]
+	us = 'Home'
+	uslink = '/'
+	vs = categoria.titulo
+	ws = page
+	context = {
+		'categoria':categoria,
+		'categorias':categorias,
+		'post':post,
+		'link':link,
+		'link2':link2,
+		'us':us,
+		'uslink':uslink,
+		'vs':vs,
+		'ws':ws,
+	}
+	return render(request, 'categoria/index.html', context)
+
+
+def indexBlog(request, v):
+	if request.user.is_authenticated():
+		categorias = Categoria.objects.filter(menu=1, activo=True, bloqueo=True).order_by('titulo')
+		posts = Post.objects.filter(activo=True, bloqueo=True).order_by('-fechacreado')
+		paginator = Paginator(posts, 10)
+		try:
+			page = int(v)
+		except:
+			page = 1
+		try:
+			post = paginator.page(page)
+		except (EmptyPage, InvalidPage):
+			post = paginator.page(paginator.num_pages)
+		link2 = Post.objects.filter(activo=True, bloqueo=True).order_by('-vistas')[:10]
+		us = 'Home'
+		uslink = '/'
+		vs = 'Blog'
+		ws = page
+		context = {
+			'categorias':categorias,
+			'post':post,
+			'link2':link2,
+			'us':us,
+			'uslink':uslink,
+			'vs':vs,
+			'ws':ws,
+		}
+		return render(request, 'blog/index.html', context)
+	else:
+		return HttpResponseRedirect('/')
+
+
+def indexCategoria(request, u, v):
+	categoria = Categoria.objects.get(slug=u, activo=True, bloqueo=True)
+	categorias = Categoria.objects.filter(menu=1, activo=True, bloqueo=True).order_by('titulo')
+	posts = Post.objects.filter(categoria=categoria.id, activo=True, bloqueo=True).order_by('-fechacreado')
+	paginator = Paginator(posts, 10)
+	try:
+		page = int(v)
+	except:
+		page = 1
+	try:
+		post = paginator.page(page)
+	except (EmptyPage, InvalidPage):
+		post = paginator.page(paginator.num_pages)
+	link = Post.objects.filter(categoria=categoria.id, activo=True, bloqueo=True).order_by('-vistas')[:10]
+	link2 = Post.objects.filter(activo=True, bloqueo=True).order_by('-vistas')[:10]
+	us = 'Home'
+	uslink = '/'
+	vs = categoria.titulo
+	ws = page
+	context = {
+		'categoria':categoria,
+		'categorias':categorias,
+		'post':post,
+		'link':link,
+		'link2':link2,
+		'us':us,
+		'uslink':uslink,
+		'vs':vs,
+		'ws':ws,
+	}
+	return render(request, 'categoria/index.html', context)
